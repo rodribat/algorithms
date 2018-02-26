720f2d3e3a1d11ed897642e658c69c13624a9061
rodribat <rodribat@hotmail.com>
2018-02-26 18:40:15 -0300

    Hash table ransom note.

---
+++ ransom_note.cpp
@@ -0,0 +1,62 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    
+    std::map<string, int> dict;
+    for (int i=0; i<magazine.size(); i++)
+        dict[magazine[i]]++;
+    
+    for (int i=0; i<ransom.size(); i++)
+    {
+        std::map<string, int>::iterator it = dict.find(ransom[i]);
+        
+        if (it != dict.end() && it->second > 0) //if the word is available
+            dict[ransom[i]]--;
+        else //the word not exists
+        return false;
+    }
+    
+    return true;
+}
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
