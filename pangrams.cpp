2b08ab9e54ff54d1e4b9abf498ac1fde0c5a64f9
Rodrigo Batista <rodribat@gmail.com>
2018-04-01 09:52:49 -0300

    Pangrams.

---
+++ pangrams.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pangrams function below.
+ */
+string pangrams(string s) {
+    map<char, int> hist;
+    
+    for (int i=0; i<s.size(); i++)
+        if (s[i] != ' ')
+            hist[tolower(s[i])]++;
+    
+    if (hist.size() == 26)
+        return "pangram";
+    else
+        return "not pangram";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
