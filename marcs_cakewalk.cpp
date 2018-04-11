c14c54c7a4dab7198ea8b53db89a26797d6aec26
rodribat <rodribat@hotmail.com>
2018-04-11 14:46:38 -0300

    Marcs Cakewalk.

--- marcs_cakewalk.cpp
+++ marcs_cakewalk.cpp
@@ -17,7 +17,7 @@ long marcsCakewalk(vector <int> calorie) {
     sort(calorie.begin(), calorie.end(), desc);
     
     for (int i=0; i<calorie.size(); i++)
-        miles += calorie[i]*pow(2,i);
+        miles += 1LL*calorie[i]<<i; //force implicit conversion
     
     return miles;
 }
