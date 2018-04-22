e81d929c1c74132d82a6ee6592fa89f537f071f0
Rodrigo Batista <rodribat@gmail.com>
2018-04-22 11:34:08 -0300

    Lisa's workbook.

--- lisas_workbook.cpp
+++ lisas_workbook.cpp
@@ -2,19 +2,32 @@
 
 using namespace std;
 
-int workbook(int n, int k, vector <int> arr) {
-    int page = 0;
+int workbook(int n, int k, vector<int> arr) {
+    int page = 1;
     int specials = 0;
     
-    for (int i=1; i<=n; i++)
+    for (int i=0; i<n; i++)
+    {
+        int problems_in_page = 0;
         for (int j=1; j<=arr[i]; j++)
         {
-            if (j%k==1)
-                page++;
-            
             if (j==page)
                 specials++;
+            
+            problems_in_page++;
+            
+            //cout << "p: " << page << " n: " << j << (j==page?" *":"") << endl;
+            
+            if (problems_in_page==k)
+            {
+                page++;
+                problems_in_page = 0;
+            }
         }
+        
+        if (problems_in_page > 0)
+            page++;
+    }
     
     return specials;
 }
